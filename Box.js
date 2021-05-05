2f1d78146dc449db1e6d5f755539163e58ab86ae
irinsajan <irinsajan@gmail.com>
2021-05-05 07:44:44 +0530

    Corrected

--- Box.js
+++ Box.js
@@ -5,14 +5,16 @@ class Box {
         }
         this.body = Bodies.rectangle(x,y,width,height,options);
         World.add(world, this.body);
+        this.width = width;
+        this.height = height;
     }
-  display(){
-  var pos =this.body.postion;
-  rectMode(CENTER) ;
-  fill(255)
-  rect(pos.x,pos,y,this.width,this.height)
 
-  }  
+    display(){
+        var pos =this.body.position;
+        rectMode(CENTER) ;
+        fill("red");
+        rect(pos.x,pos.y,this.width,this.height);
+    }  
 }
 
 
